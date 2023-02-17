03c4bfe151abb703db70842f53ac968bfcdcdc96
Shivam Veerabudren <shivamveerabudren@gmail.com>
2023-02-17 16:15:51 -0500

    changed responsive modifier

--- src/pages/index.js
+++ src/pages/index.js
@@ -76,7 +76,7 @@ export default function Home() {
         <Link href='/schedule'>Schedule</Link>
 
         {/**Picture Grid */}
-        <div className='grid text-black grid-cols-2 lg:grid-cols-4  md:grid-cols-3 sm:grid-cols-2'>
+        <div className='grid text-black grid-cols-2 lg:grid-cols-4  md:grid-cols-3 sm:grid-cols-3'>
           {
             pictureGrid.map((item, index) => {
               return <PictureCard key={index} path={item.src} caption={item.caption} />
