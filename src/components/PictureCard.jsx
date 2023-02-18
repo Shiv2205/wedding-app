c36572c4cd46d1efbf91ad759fb2e681391a1071
Shivam Veerabudren <shivamveerabudren@gmail.com>
2023-02-18 14:32:46 -0500

    Fixed slow loading for images

--- src/components/PictureCard.jsx
+++ src/components/PictureCard.jsx
@@ -4,7 +4,7 @@ const PictureCard = ({ path, caption }) => {
     return (
     <div className="p-2">
         <div className="max-w-sm rounded bg-white overflow-hidden shadow-2xl cursor-pointer">
-          <img className="w-full" src={path} alt="Preview" />
+          <Image className="w-full" width={700} height={500} src={path} alt="Preview" priority />
 
           <div className="px-6 py-4">
             <div className="font-bold text-xl mb-2">Shivam</div>
