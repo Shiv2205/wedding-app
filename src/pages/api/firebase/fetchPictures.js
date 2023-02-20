06bec64f7ed996072647c77b67f5cc02be002fd0
Shivam Veerabudren <shivamveerabudren@gmail.com>
2023-02-19 22:32:31 -0500

    ..

--- src/pages/api/firebase/fetchPictures.js
+++ src/pages/api/firebase/fetchPictures.js
@@ -3,7 +3,7 @@ import { getDatabase, ref, onValue } from "firebase/database";
 
 const fetchPictures = async () => {
  let pictures;
-  const db = getDatabase(firebaseApp());
+  const db = getDatabase(firebaseApp(), "https://wedding-app-1b728-default-rtdb.europe-west1.firebasedatabase.app");
   const pictureRef = ref(db, "pictureGrid/");
   onValue(pictureRef, (snapshot) => {
     pictures = snapshot.val();
