603853c0c15fa09a8ee33634a91af93c509865d1
Shivam Veerabudren <shivamveerabudren@gmail.com>
2023-02-22 17:39:32 -0500

    Ignoring eslint errors during build

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   images:{
     domains: ['wallpapers.com', 'media.zenfs.com']
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   }
 }
 
