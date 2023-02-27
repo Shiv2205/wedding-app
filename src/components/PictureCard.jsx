a0d538e89ea56e613b4cfbfb5df461a9c668005d
GK2205 <92755248+Shiv2205@users.noreply.github.com>
2023-02-27 11:22:39 -0500

    Gallery design improvement

--- src/components/PictureCard.jsx
+++ src/components/PictureCard.jsx
@@ -5,11 +5,13 @@ import cn from '@/util/cn';
 function PictureCard({ path, count }) {
   const randomStyle = () => {
     const decision = Math.round(Math.random() * 2);
-    if (decision === 1) {
-      return 'row-span-2 ';
-    }
-    if(decision === 2) {
-      return 'col-span-2';
+    if(count % 2 !== 0) {
+      if (decision === 1) {
+        return 'row-span-2 ';
+      }
+      if(decision === 2) {
+        return 'col-span-2';
+      }
     }
     return 'col-auto row-auto';
   };
@@ -21,9 +23,10 @@ function PictureCard({ path, count }) {
   });
 
   return (
-    <div className={cn('p-2 h-full w-full', viewport)}>
+    <div className={cn('p-2 h-full w-full  ', viewport)}>
       <div
-        className="h-full rounded bg-white overflow-hidden shadow-2xl cursor-pointer "
+        className="h-full rounded-lg bg-white overflow-hidden 
+          shadow-2xl shadow-black cursor-pointer "
       >
         <Image className="w-full h-full" width={2000} height={1000} loading="eager" priority src={path} alt="Preview" />
 
